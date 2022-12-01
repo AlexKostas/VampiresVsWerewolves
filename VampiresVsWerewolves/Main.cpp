36813c13a8e02f62d25c0c061e980c56e38c5e29
dimitris_an <sdi2100002@di.uoa.gr>
2022-12-01 22:44:37 +0200

    Added real time clock

--- VampiresVsWerewolves/Main.cpp
+++ VampiresVsWerewolves/Main.cpp
@@ -1,21 +1,31 @@
-﻿// VampiresVsWerewolves.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <ctime>
 #include "Game.h"
 
+using namespace std;
+
+const int frameRate=5;
+
 int main()
-{
+{	
+	//TODO GET DIMENSIONS FROM USER
 	Game game(10, 10);
+	
+	float frameTime = (1.0 / frameRate) * CLOCKS_PER_SEC;
+
+
+	//TODO WHILE THE GAME IS NOT OVER
+	while (true) {
+
+		clock_t now = clock();//this gets the time in sec.
+		while (true)
+		{
+			while (clock() - now < frameTime);
+			//call your function
+			now = clock();
+			system("cls");
+			game.Update();
+		}
+	}
+	
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
