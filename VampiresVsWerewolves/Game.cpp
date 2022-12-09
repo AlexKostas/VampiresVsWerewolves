ea00c358119e02c2616b64a02a9e2f91a1127649
Alex Kostas <k.alexandros299@gmail.com>
2022-12-09 14:55:10 +0200

    Refactored game

--- VampiresVsWerewolves/Game.cpp
+++ VampiresVsWerewolves/Game.cpp
@@ -16,43 +16,21 @@
 #define KEY_LEFT 75
 #define KEY_RIGHT 77
 #define ESC 27
+#define MIN_BOARD_SIZE 4
 
 using std::cout;
 using std::endl;
 
 Game::Game(int row, int column): werewolvesCount((row*column)/15), vampiresCount((row*column)/15)
 {
-	//TODO CHECK IF THE ROW,COLUMN ARE VALID
-	if (row < 2 || column < 2)
+	if (row < MIN_BOARD_SIZE || column < MIN_BOARD_SIZE)
 		throw invalid_argument("Invalid Board Size\n");
 	
 	map = new Map(row, column);
 
-	for (int i = 0;i < werewolvesCount;i++) {
-		int row,column;
-
-		getValidRandomCoordinates(row, column);
-
-		Werewolf* werewolf = new Werewolf(row,column, this);
-
-		map->UpdateEntityPosition(row, column,MapCellType::werewolf);
-
-		werewolves.push_back(werewolf);
-		entities.push_back(werewolf);
-	}
-
-	for (int i = 0;i < vampiresCount;i++) {
-		int row, column;
-		
-		getValidRandomCoordinates(row, column);
-
-		Vampire* vampire = new Vampire(row, column, this);
-
-		map->UpdateEntityPosition(row, column, MapCellType::vampire);
-
-		vampires.push_back(vampire);
-		entities.push_back(vampire);
-	}
+	createWerewolves();
+	createVampires();
+	createAvatar();
 }
 
 Game::~Game()
@@ -69,104 +47,34 @@ Game::~Game()
 
 void Game::Run()
 {
-
 	float frameTime = (1.0 / frameRate) * CLOCKS_PER_SEC;
 	assert(frameTime >= 0);
 
-	bool supportsWerewolf;
-	do {
-		cout << "Choose your team. V for vampires or W for werewolves" << endl;
-
-		string input;
-		cin >> input;
-
-		if (input == "V" || input=="v") {
-			supportsWerewolf = false;
-			break;
-		}
-		else if (input == "W"|| input=="w") {
-			supportsWerewolf = true;
-			break;
-		}
-	} while (true);
-
-	int newRow, newColumn;
-
-	getValidRandomCoordinates(newRow, newColumn);
-
-	avatar = new Avatar(newRow, newColumn, this, supportsWerewolf);
-	entities.push_back(avatar);
-
-	map->UpdateEntityPosition(newRow, newColumn, MapCellType::avatar);
-
-
 	clock_t now = clock();
-	// GAME LOOP
-	while (!IsOver())
-	{
-		while (clock() - now < frameTime);
 
+	// GAME LOOP
+	while (!isOver())
+	{
+		// Wait for next frame
+		while (clock() - now < frameTime);
 		now = clock();
 		
-		bool gameTerminatedByPlayer = false;
-		if (_kbhit()) {
-			int c;
-			switch ((c = _getch())) {
+		// If player asked to exit handleInput returns true
+		// so we need to exit the game loop
+		if (handleInput()) break;
 
-			case KEY_UP:
-				avatar->GoUp();
-				break;
-
-			case KEY_DOWN:
-				avatar->GoDown();
-				break;
-
-			case KEY_LEFT:
-				avatar->GoLeft();
-				break;
-
-			case KEY_RIGHT:
-				avatar->GoRight();
-				break;
-
-			case ESC:
-				gameTerminatedByPlayer = true;
-				break;
-
-			case '\t':
-				isPaused = !isPaused;
-				cout << "\nGAME IS PAUSED! Press Tab to continue..." << endl;
-
-				string team = (avatar->SupportsWerewolves()) ? "Werewolves" : "Vampires";
-
-				cout << "You support " <<  team << "!" << endl;
-				cout << "Number of Vampires: " << vampires.size() << endl;
-				cout << "Number of Werewolves: " << werewolves.size() << endl;
-				break;
-			}
-		}
-
-		if (gameTerminatedByPlayer) break;
-
+		// Update the state of the game as long
+		// as it is not paused
 		if (!isPaused) { 
 			system("cls");
-			Update(); 
+			update(); 
 		}
 	}
-	
-	if (werewolves.size() == 0 && vampires.size() == 0) {
-		cout << "\nITS A DRAW!!";
-	}
-	else if (vampires.size() == 0) {
-		cout << "\nVAMPIRES WON!!" << endl;
-	}
-	else if (werewolves.size() == 0)
-		cout << "\nWEREWOLVES WON!!" << endl;
-	else
-		cout << "GAME TERMINATED EARLY" << endl;
+
+	displayEndOfGameMessages();
 }
 
-void Game::Update()
+void Game::update()
 {
 	for (auto& entity : entities) {
 		int oldRow, oldColumn;
@@ -205,7 +113,55 @@ void Game::Update()
 	
 }
 
-bool Game::IsOver()
+/// <summary>
+/// Responsible for handling input from the user. Returns true if the user
+/// asked for the game to end.
+/// </summary>
+bool Game::handleInput()
+{
+	bool gameTerminatedByPlayer = false;
+
+	if (_kbhit()) {
+		int c;
+		switch ((c = _getch())) {
+
+		case KEY_UP:
+			avatar->GoUp();
+			break;
+
+		case KEY_DOWN:
+			avatar->GoDown();
+			break;
+
+		case KEY_LEFT:
+			avatar->GoLeft();
+			break;
+
+		case KEY_RIGHT:
+			avatar->GoRight();
+			break;
+
+		case ESC:
+			gameTerminatedByPlayer = true;
+			break;
+
+		case '\t':
+			isPaused = !isPaused;
+			cout << "\nGAME IS PAUSED! Press Tab to continue..." << endl;
+
+			string team = (avatar->SupportsWerewolves()) ? "Werewolves" : "Vampires";
+
+			cout << "You support " << team << "!" << endl;
+			cout << "Number of Vampires: " << vampires.size() << endl;
+			cout << "Number of Werewolves: " << werewolves.size() << endl;
+			break;
+		}
+	}
+
+	return gameTerminatedByPlayer;
+}
+
+bool Game::isOver()
 {
 	return vampires.size() == 0 || werewolves.size() == 0;
 }
@@ -220,13 +176,12 @@ vector<pair<int, int>> Game::GetAvailableDiagonalNeighboringCells(int row, int c
 	return map->GetAvailableDiagonalNeighboringCells(row, col);
 }
 
-int Game::GetRows()
+int Game::GetRows() const
 {
-
 	return map->GetRow();
 }
 
-int Game::GetColumns()
+int Game::GetColumns() const
 {
 	return map->GetColumn();
 }
@@ -237,12 +192,72 @@ void Game::UpdateEntityPosition(int oldRow, int oldColumn, int newRow, int newCo
 }
 
 bool Game::HasPotion(int row, int col) const
-{
-	
+{	
 	return map->HasPotion(row,col);
 }
 
-void Game::getValidRandomCoordinates(int& row, int& column)
+void Game::createVampires()
+{
+	for (int i = 0; i < vampiresCount; i++) {
+		int row, column;
+
+		getValidRandomCoordinates(row, column);
+
+		Vampire* vampire = new Vampire(row, column, this);
+
+		map->UpdateEntityPosition(row, column, MapCellType::vampire);
+
+		vampires.push_back(vampire);
+		entities.push_back(vampire);
+	}
+}
+
+void Game::createWerewolves()
+{
+	for (int i = 0; i < werewolvesCount; i++) {
+		int row, column;
+
+		getValidRandomCoordinates(row, column);
+
+		Werewolf* werewolf = new Werewolf(row, column, this);
+
+		map->UpdateEntityPosition(row, column, MapCellType::werewolf);
+
+		werewolves.push_back(werewolf);
+		entities.push_back(werewolf);
+	}
+}
+
+void Game::createAvatar()
+{
+	bool supportsWerewolf;
+	do {
+		cout << "Choose your team. V for vampires or W for werewolves" << endl;
+
+		string input;
+		cin >> input;
+
+		if (input == "V" || input == "v") {
+			supportsWerewolf = false;
+			break;
+		}
+		else if (input == "W" || input == "w") {
+			supportsWerewolf = true;
+			break;
+		}
+	} while (true);
+
+	int newRow, newColumn;
+
+	getValidRandomCoordinates(newRow, newColumn);
+
+	avatar = new Avatar(newRow, newColumn, this, supportsWerewolf);
+	entities.push_back(avatar);
+
+	map->UpdateEntityPosition(newRow, newColumn, MapCellType::avatar);
+}
+
+void Game::getValidRandomCoordinates(int& row, int& column) const
 {
 	int tempRow, tempColumn;
 
@@ -252,4 +267,16 @@ void Game::getValidRandomCoordinates(int& row, int& column)
 
 	row = tempRow;
 	column = tempColumn;
+}
+
+void Game::displayEndOfGameMessages() const
+{
+	if (werewolves.size() == 0 && vampires.size() == 0)
+		cout << endl << "ITS A DRAW!!";
+	else if (vampires.size() == 0)
+		cout << endl << "VAMPIRES WON!!" << endl;
+	else if (werewolves.size() == 0)
+		cout << endl << "WEREWOLVES WON!!" << endl;
+	else
+		cout << endl << "GAME TERMINATED EARLY" << endl;
 }
